c241cb87d5a3992b60f6beafe4442d2a0dda8d02
Anthony Papoutsis <anthony8121999@gmail.com>
2023-03-06 19:00:49 +1100

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Nav from "./components/Nav";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Books from "./pages/Books";
